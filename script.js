ffd4211cab468336a515335bda5461f811bc2545
Sara Rincon <sararincon@MacBook-Air-de-Sara.local>
2022-04-28 12:10:07 -0400

    Done

--- script.js
+++ script.js
@@ -7,61 +7,65 @@ http
 .createServer(function (req, res) {
 
 const params = url.parse(req.url, true).query
-const nombreArchvio = params.archivo
-const contenido = params.contenido
-const nuevoNombre = params.nuevoNombre
+const {archivo} = params
+const {contenido} = params
+const {nuevoNombre} = params
 
 //Crear
 if (req.url.includes('/crear')) {
-    fs.writeFile(nombreArchvio, contenido, (err, data) => {
+    fs.writeFile(archivo, contenido, 'utf-8', (err, data) => {
+      const year = new Date().getFullYear();
+      const month = new Date().getMonth () 
+      const day = new Date().getDay();
+      const date = `${day}/${month < 10 ? '0' + month : month}/${year}`;
+      console.log({date});
       if (err){
-        res.write('El archivo no se pudo crear.')
-        res.end()
+
+        res.end('El archivo no se pudo crear.')
       }else {
-        res.write('Archivo creado con éxito!')
-        res.end()
+        
+        res.end('Archivo creado con exito!')
     }
   })  
 }
 
 //Leer
 if (req.url.includes('/leer')) {
-    fs.readFile(nombreArchvio, (err, data) => {
+    fs.readFile(archivo, (err, data) => {
       if (err){
-        res.write('El archivo no se puede leer.')
-        res.end()
+        
+        res.end('El archivo no se puede leer.')
       }else{
-         res.write(data)
-         res.end()
+         
+         res.end(data)
      } 
   })
 } 
 
 //Renombrar
 if (req.url.includes('/renombrar')) {
-  fs.rename(nombreArchvio, nuevoNombre, (err, data) => {
+  fs.rename(archivo, nuevoNombre, (err, data) => {
     if (err){
-      res.write('Error al renombrar el archivo.')
-      res.end()
+      res.end('Error al renombrar el archivo.')
     }else {
-     res.write(`El archivo: ${nombreArchvio} ha sido renombrado con exito como: ${nuevoNombre}`)
-     res.end()
+     res.end(`El archivo: ${archivo} ha sido renombrado con exito como: ${nuevoNombre}`)
     }
    })
   }
 
   //Eliminar    
 if (req.url.includes('/eliminar')) {
-    fs.unlink(nombreArchvio, (err, data) => {
+  console.log(`Se esta procesando la eliminación del ${archivo}`);
+  function eliminar(){
+    fs.unlink(archivo, (err, data) => {
       if (err){
-        res.write('Error al eliminar el archivo.')
-        res.end()
+      res.end('Error al eliminar el archivo.')
       }else{
-      res.write(`Archivo ${nombreArchvio} eliminado con éxito`)
-      res.end()
+      res.end(`Archivo ${archivo} eliminado con exito`)
      }
    })
-  }
-  
+  }setTimeout(eliminar, 3000);
+}
+
 })
 .listen(8080, () => console.log('Server ON'))
